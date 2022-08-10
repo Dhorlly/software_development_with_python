32cc52f894b5146df514fae0349082d9ced4bee1
Opetunde <dhorllyibi@gmail.com>
2022-08-11 00:28:59 +0300

    A simple Personal Income Tax Calculator

---
+++ pit.py
@@ -0,0 +1,16 @@
+##
+#Personal Income Tax Calculator
+##
+
+print("Personal Income Tax Calculator")
+income = float(input("Enter your income: "))
+
+if income <= 85528:
+    tax = round(((income * 18)/100) - 556.02, 0)
+    if tax < 0:
+        print("The tax is: 0.0 thalers")
+    else:
+        print("The tax is: ", tax, "thalers")
+else:
+    tax = round((((income - 85528) * 32/100) + 14839.02, 0)
+        print("The tax is:", tax, "thalers")
